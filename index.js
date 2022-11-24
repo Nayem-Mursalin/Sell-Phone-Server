f344c963b13fa6aaa2f5da6b5c1d4b42cb88af98
Nayem-Mursalin <nayem.mursalin19@gmail.com>
2022-11-24 17:04:47 +0600

    Connect to DB and env

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5500;
+require('dotenv').config();
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.c8rri4v.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+
+    }
+    finally {
+
+    }
+} run().catch(error => console.error(error));
+
 
 app.get('/', async (req, res) => {
     res.send('My Server is Running ')
