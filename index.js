3f1361bda0b2cb666ea5cf9613b439587c912d18
Nayem-Mursalin <nayem.mursalin19@gmail.com>
2022-11-30 19:24:34 +0600

    Admin show

--- index.js
+++ index.js
@@ -97,6 +97,26 @@ async function run() {
             res.send(result);
         });
 
+        app.get('/users/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email }
+            const user = await usersCollection.findOne(query);
+            res.send({ isAdmin: user?.role === 'admin' });
+        })
+
+        app.put('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.send(result);
+        });
+
 
 
     }
