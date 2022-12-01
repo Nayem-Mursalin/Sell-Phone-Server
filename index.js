6bda9750e60fd13f144922551b624c7d9ddd7ccb
Nayem-Mursalin <nayem.mursalin19@gmail.com>
2022-12-01 20:34:10 +0600

    payment 2

--- index.js
+++ index.js
@@ -143,8 +143,8 @@ async function run() {
         });
 
         app.post('/create-payment-intent', async (req, res) => {
-            const booking = req.body;
-            const price = booking.price;
+            const order = req.body;
+            const price = order.price;
             const amount = price * 100;
 
             const paymentIntent = await stripe.paymentIntents.create({
@@ -158,6 +158,21 @@ async function run() {
                 clientSecret: paymentIntent.client_secret,
             });
         });
+
+        app.post('/payments', async (req, res) => {
+            const payment = req.body;
+            const result = await paymentsCollection.insertOne(payment);
+            const id = payment.bookingId
+            const filter = { _id: ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+                    paid: true,
+                    transactionId: payment.transactionId
+                }
+            }
+            const updatedResult = await bookingsCollection.updateOne(filter, updatedDoc)
+            res.send(result);
+        })
 
 
 
